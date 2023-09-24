df1bcaef9154bc2976efabd82ee622a2296fe085
JGavk <119267651+JGavk@users.noreply.github.com>
2023-09-24 17:59:51 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,31 @@
+package visual;
+import java.awt.Canvas;
+
+
+/**
+ *
+ * @author juanp
+ */
+public class Game extends Canvas implements Runnable { /* Clase game más herencia de canvas */
+    
+
+    
+    public Game(){
+        new Window(WIDTH, HEIGHT, "Geometric figures", this); 
+    }
+    
+    public synchronized void start(){
+
+
+}
+    
+   
+    public void run() {
+
+    }
+    
+
+
+    
+}
+/* Primer vista de la ventana */
